6f7502c1da5273f252235b2624dc349e58c0bacf
Melinda <codegirl78@gmail.com>
2021-09-07 12:10:47 -0500

    Added list

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<string> Events = new List<string>();
+            Events.Add("Code With Pride");
+            Events.Add("Apple WWDC");
+            Events.Add("Strange Loop");
+
+            ViewBag.events = Events;
+
+            return View();
+        }
+    }
+}

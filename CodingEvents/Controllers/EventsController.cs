d3fd939867c48da6f318fd2469b840314d75950b
Melinda <codegirl78@gmail.com>
2021-09-07 19:59:59 -0500

    Completed exercises

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -10,11 +10,15 @@ namespace CodingEvents.Controllers
     {
         //static private List<string> Events = new List<string>();
         private static Dictionary<string, string> Events = new Dictionary<string, string>();
-        
+
         // GET: /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
+/*          Events.Add("Strange Loop");
+            Events.Add("Grace Hopper");
+            Events.Add("Code with Pride");*/
+
             ViewBag.events = Events;
 
             return View();

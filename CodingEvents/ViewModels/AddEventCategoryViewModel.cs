76abc391b2538b099a173aaf3d54599153ee57f9
Melinda <codegirl78@gmail.com>
2021-10-13 19:07:22 -0500

    Added a new view model

---
+++ CodingEvents/ViewModels/AddEventCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodingEvents.ViewModels
+{
+    public class AddEventCategoryViewModel
+    {
+        [Required, StringLength(20, MinimumLength = 3, ErrorMessage = "Must be between 3 and 20 characters.")]
+        public string Name { get; set; }
+    }
+}
